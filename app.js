830c5fac13b0a9dc515f4805f8f765cbe3e4f432
Pinky Gupta <64944374+PinkyGuptaa@users.noreply.github.com>
2026-03-03 15:12:25 +0530

    ui code
    Removed commented-out code and cleaned up the format. Updated the URL shortening logic and improved error handling.

--- app.js
+++ app.js
@@ -1,42 +1,7 @@
 /**
  * @Author: Pinky Gupta 
- * @Date:   2026-02-26T16:10:09+05:30
- * @Last Modified by:   Pinky Gupta 
- * @Last Modified time: 2026-02-27T11:44:09+05:30
  */
 
-// async function shortenUrl() {
-//     const originalUrl = document.getElementById('originalUrl').value;
-//     if (!originalUrl) {
-//         alert('Please enter a URL');
-//         return;
-//     }
-
-//     try {
-//         const response = await fetch(`http://localhost:7003/api/url/shorten?originalUrl=${encodeURIComponent(originalUrl)}`, {
-//             method: 'POST'
-//         });
-
-//         if (!response.ok) throw new Error('API error');
-
-//         const data = await response.json();
-//         const shortUrlDisplay = document.getElementById('shortUrlDisplay');
-//         shortUrlDisplay.innerHTML = `
-//             Short URL: <a href="http://localhost:7003/${data.shortUrl}" target="_blank">
-//             http://localhost:7003/${data.shortUrl}</a>
-//             <button onclick="copyToClipboard('${data.shortUrl}')">Copy</button>
-//         `;
-//     } catch (err) {
-//         alert('Error: ' + err.message);
-//     }
-// }
-
-// function copyToClipboard(shortUrl) {
-//     const fullUrl = `http://localhost:7003/${shortUrl}`;
-//     navigator.clipboard.writeText(fullUrl).then(() => {
-//         alert('Copied to clipboard: ' + fullUrl);
-//     });
-// }
 async function shortenUrl() {
     const originalUrl = document.getElementById("originalUrl").value;
     if (!originalUrl) {
@@ -80,4 +45,5 @@ function copyToClipboard(text) {
     navigator.clipboard.writeText(text)
         .then(() => alert("Copied: " + text))
         .catch(() => alert("Copy failed"));
+
 }
